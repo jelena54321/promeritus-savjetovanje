0dae6ad03484a1f2e30eb2916cdad1796e65ca77
Jelena Šarić <jelenaa.saric@gmail.com>
2020-11-30 03:10:25 +0100

    Added initial ingration for the 'About Us' page.

---
+++ src/strings/strings.hr.js
@@ -0,0 +1,26 @@
+strings = {
+    about_us_page_title: "O nama | Promeritus savjetovanje",
+
+    about_us_title: "+ O NAMA",
+
+    home: "Početna",
+    about_us: "O nama",
+    vision_and_mission: "Vizija i misija",
+    services: "Usluge",
+    contact: "Kontakt",
+
+    about_us_content: 
+    `
+    Promeritus savjetovanje ima cijeli spektar usluga savjetovanja s ciljem da vam omogući brzo i jednostavno ostvarivanje vrhunske usluge, u skladu s vlastitim potrebama. Pri tom nastojimo udružiti lokalna znanja i iskustvo iznimnih internacionalnih stručnjaka u našoj mreži, kako bi našim klijentima isporučili vrhunske usluge savjetovanja i edukacije.
+
+    Uspjeh je moguć uz pristup koji usmjerava pozornosti na pojedinca sa svim njegovim različitostima i potiče primjenu inovacija i naprednih znanstvenih spoznaja. Uz to je nužan i standardiziran sustav osiguranja kvalitete.
+    
+    Promeritus savjetovanje je utemeljeno 2010. godine kao tvrtka za savjetovanje i usluge. Osnovna djelatnost tvrtke je savjetovanje vezano uz zdravstvo, a aktivnosti i usluge su usmjerene prema područjima savjetovanja vezanim uz farmaceutsku industriju te prema ostalim klijentima.
+    
+    Udovoljavajući zahtjevima klijenata, dali smo prednost ostvarivanju visoko specijaliziranih medicinskih usluga uz stručan i cjelovit pristup. Dio naših aktivnosti je vezan uz zdravstveno prosvjećivanje i educiranje, potičući time aktivnu brigu o zdravlju i unaprjeđenje kvalitete života.
+    
+    Kako kvalitetna zdravstvena usluga podrazumijeva partnerski odnos između bolesnika i zdravstvenog djelatnika, što doprinosi zadovoljstvu i sigurnosti na obje strane i uspostavi potpunog povjerenja i razumijevanja, naš cilj je usmjeravati zdravstvene djelatnike i ustanove prema učinkovitoj zdravstvenoj skrbi u skladu sa zahtjevima naprednog sustava osiguranja kvalitete.
+    
+    Naša tvrtka, u skladu s potrebama klijenata, surađuje s vrhunskim stručnjacima iz raznih područja. Pri tome inzistiramo na timskom radu. Koristeći se različitostima unutar tima, stvaramo više kreativnih rješenja koja dovode i do boljih rezultata. Time osiguravamo visoko kvalitetnu skrb i uslugu korisnicima u skladu s našim kriterijima.
+    `
+}
