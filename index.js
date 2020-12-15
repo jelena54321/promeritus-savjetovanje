b038da383843b86cc00fc827eec010f8faa11aaf
Jelena Šarić <jelenaa.saric@gmail.com>
2020-12-15 22:52:34 +0100

    Made sure that header is added dynamically.

--- index.js
+++ index.js
@@ -1,45 +1,15 @@
-$(document).ready(pageLoaded);
+$(document).ready(homePageLoaded);
 
-function getCurrentLanguage() {
-    if (window.location.href.indexOf('?') == -1) {
-        return "hr";
-    } else {
-        return "en";
-    }
-}
-
-function pageLoaded() {
+function homePageLoaded() {
     let script = document.createElement("script");
-    script.src = "src/strings/strings." + getCurrentLanguage() + ".js";
-    script.onload = loadStrings;
+    script.src = `src/strings/strings.${getCurrentLanguage()}.js`;
+    script.onload = loadHomeStrings;
     document.head.appendChild(script);
 }
 
-function loadStrings() {
+function loadHomeStrings() {
     document.title = strings.about_us_page_title;
 
-    document.getElementById("home-menu-item").innerText += strings.home;
-    document.getElementById("about-us-menu-item").innerText += strings.about_us;
-    document.getElementById("vision-and-mission-menu-item").innerText += strings.vision_and_mission;
-    document.getElementById("services-menu-item").innerText += strings.services;
-    document.getElementById("contact-menu-item").innerText += strings.contact;
-
-    let language = getCurrentLanguage();
-    document.getElementById("selected-language").innerText += language.toUpperCase();
-    document.getElementById("unselected-language").innerText += (language == "hr" ? "en" : "hr").toUpperCase();
-
     document.getElementById("about-us-title").innerText += strings.about_us_title;
-
     document.getElementById("about-us-content").innerText += strings.about_us_content;
-}
-
-function changeLanguage() {
-    let url = window.location.href;
-    if (getCurrentLanguage() == "hr") {
-        url += "?lang=en";
-    } else {
-        url = url.substring(0, url.indexOf('?'));
-    }
-
-    window.location.href = url;
 }
---
+++ shared.js
@@ -0,0 +1,95 @@
+$(document).ready(pageLoaded);
+
+linker = {
+    "index.html": "home",
+    "about_us.html": "about-us",
+    "vission_and_mission.html": "vision-and-mission",
+    "services.html": "services",
+    "contact.html": "contact"
+}
+
+function pageLoaded() {
+    showHeader();
+
+    let script = document.createElement("script");
+    script.src = `src/strings/strings.${getCurrentLanguage()}.js`;
+    script.onload = loadStrings;
+    document.head.appendChild(script);
+}
+
+function getCurrentLanguage() {
+    if (window.location.href.indexOf('?') == -1) {
+        return "hr";
+    } else {
+        return "en";
+    }
+}
+
+function loadStrings() {
+    document.getElementById("home-menu-item").innerText += strings.home;
+    document.getElementById("about-us-menu-item").innerText += strings.about_us;
+    document.getElementById("vision-and-mission-menu-item").innerText += strings.vision_and_mission;
+    document.getElementById("services-menu-item").innerText += strings.services;
+    document.getElementById("contact-menu-item").innerText += strings.contact;
+
+    let language = getCurrentLanguage();
+    document.getElementById("selected-language").innerText += language.toUpperCase();
+    document.getElementById("unselected-language").innerText += (language == "hr" ? "en" : "hr").toUpperCase();
+}
+
+function showHeader() {
+    document.getElementById('header').innerHTML +=
+    `
+    <div class="header-content">
+
+            <div class="logo-container">
+                <img width="180px" src="src/images/logo.png" alt="logo">
+            </div>
+
+            ${getPageMenu()}
+
+            <div class="language-menu">
+                <button id="selected-language" class="language-menu-button" type="button"></button>
+                <div class="language-menu-content">
+                  <a id="unselected-language" href="javascript:changeLanguage();"></a>
+                </div>
+            </div>
+        
+    </div>
+    `
+}
+
+function getPageMenu() {
+    let currentPageURL = window.location.href;
+    let startIndex = currentPageURL.lastIndexOf("/") + 1;
+    let endIndex = currentPageURL.indexOf("?");
+    let currentPage = endIndex == -1 ? 
+        currentPageURL.substring(startIndex) : 
+        currentPageURL.substring(startIndex, endIndex);
+
+    let html = "<div class='page-menu'>"
+    for (let file in linker) {
+        html += 
+        `
+        <button 
+            id='${linker[file]}-menu-item' 
+            class=${currentPage == file ? 'selected-menu-item' : 'unselected-menu-item'} 
+            type='button'>
+        </button>
+        `
+    }
+    html += "</div>"
+
+    return html;
+}
+
+function changeLanguage() {
+    let url = window.location.href;
+    if (getCurrentLanguage() == "hr") {
+        url += "?lang=en";
+    } else {
+        url = url.substring(0, url.indexOf('?'));
+    }
+
+    window.location.href = url;
+}
